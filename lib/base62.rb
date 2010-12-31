47af5cd12f8ecd4deda84d9b16e5f77964aaadd2
Pablo <pablohstc@gmail.com>
2010-12-31 12:16:05 -0200

    Adding FIXME

--- lib/base62.rb
+++ lib/base62.rb
@@ -2,10 +2,11 @@
 module MongomapperId2
   module Base62
     CHARS = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
-    # FIXME encode(1).should eql 'a' actual 'b'
-    # FIXME encode(62).should eql '99' actual 'ba'
-    # FIXME encode(3844).should eql '99' actual 'baa'
-    # FIXME encode(238328).should eql '999' actual 'baaa'
+    # The encode is skipping the first 'a'  when will change the digits size 62, 62 * 62
+    # FIXME encode(1).should eql 'a' NOT 'b'
+    # FIXME encode(62).should eql '99' NOT 'ba'
+    # FIXME encode(3844).should eql '99' NOT 'baa'
+    # FIXME encode(238328).should eql '999' NOT 'baaa'
     # See base62_spec
     def self.encode(i)
       return '0' if i == 0
